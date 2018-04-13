d22a01af3cdefc00cd21bdb794b9bb696895324d
Sviat Kuzhelev <30953392+BiosBoy@users.noreply.github.com>
2018-04-13 12:22:30 +0300

    Update EmployersBox.js

--- src/Components/Employers/EmployersBox.js
+++ src/Components/Employers/EmployersBox.js
@@ -27,7 +27,7 @@ class EmployersBox extends Component {
     }
 
     loadCommentsFromServer = () => {
-        axios.get('/employers', {withCredentials: true})
+        axios.get(':3016/employers', {withCredentials: true})
             .then(res => {
                 if (res.data.sess === undefined || res.data.sess === '') {
                     this.setState({
